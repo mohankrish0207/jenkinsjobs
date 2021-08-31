fb0bc01362dadc9c6a140fbfbae091e71294d255
mohankrish0207 <86762895+mohankrish0207@users.noreply.github.com>
2021-08-31 08:28:51 +0530

    updated hellowolrd.java
    from existed script line 3 printing statement has been changed from "welcome to jenkins sample jobs !!!!!" to this "Welcome to Jenkins Sample job using Java!!!!"

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
   public static void main(String[] args) {
-    System.out.println("Welcome to Jenkins Sample job!!!!!!");
+    System.out.println("Welcome to Jenkins Sample job using Java!!!!");
   }
 }
