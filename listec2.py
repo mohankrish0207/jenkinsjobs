1604fa3c57a0400910d21f9dcc1f34aebe4d3e9e
mohankrish0207 <86762895+mohankrish0207@users.noreply.github.com>
2021-12-20 20:38:09 +0530

    commit

--- listec2.py
+++ listec2.py
@@ -8,9 +8,7 @@ client = boto3.client('ec2',region_name = 'ap-southeast-1', aws_access_key_id = 
 myec2 = client.describe_instances()
 for printins in myec2['Reservations']:
 	for printout in printins['Instances']:
-		for printname in printout['Tags']:
-			print(printout['InstanceId'])
-			print(printout['InstanceType'])
-			print(printout['LaunchTime'])
-			print(printout['State']['Name'])
-			print(printname['Value'])
+		print(printout['InstanceId'])
+		print(printout['InstanceType'])
+		print(printout['LaunchTime'])
+		print(printout['State']['Name'])
